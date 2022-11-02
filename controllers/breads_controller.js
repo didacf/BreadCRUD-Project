ca3ec186ce3a390e8948b73b7e8905cc5b514058
Didac Fernandez <didac.fernandez@insiemecloud.net>
2022-11-01 19:01:19 -0700

    Final Project

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -7,4 +7,9 @@ breads.get('/', (req, res) => {
   res.send('This is the index at /breads')
 })
 
+// SHOW
+breads.get('/:arrayIndex', (req, res) => {
+    res.send(Bread[req.params.arrayIndex])
+  })
+  
 module.exports = breads
