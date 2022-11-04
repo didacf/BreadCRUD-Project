47601e39bc5afb1c15b51465917633efea7edb30
Didac Fernandez <didac.fernandez@insiemecloud.net>
2022-11-03 18:49:49 -0700

    sending index and show data

--- views/index.jsx
+++ views/index.jsx
@@ -3,18 +3,18 @@ const Default = require('./layouts/Default')
 
 function Index ({breads})  {
     return (
-        <Default>
-  <h2>Index Page</h2>
-  {/* <p>I have {breads[0].name} bread!</p> */}
-  {/* This is a JSX comment. */}
+    <Default>
+        <h2>Index Page</h2>
+        {/* <p>I have {breads[0].name} bread!</p> */}
+        {/* This is a JSX comment. */}
   <ul>
   {
-  breads.map((bread, index)=> {
-    return (
-      <li key={index}>
-        <a href={`/breads/${index}`}>
-          {bread.name}
-        </a>
+    breads.map((bread, index)=> {
+        return (
+        <li key={index}>
+            <a href={`/breads/${index}`}>
+                {bread.name}
+            </a>
       </li>
     )
   })
