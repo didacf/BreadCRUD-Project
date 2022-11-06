1f64e5e6c497303c46113314e9fd9d30c25a5cc6
Didac Fernandez <didac.fernandez@insiemecloud.net>
2022-11-06 10:13:47 -0800

    delete route complete

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // DEPENDENCIES
 const express = require('express')
+const methodOverride = require('method-override')
 
 // CONFIGURATION
 require('dotenv').config()
@@ -7,9 +8,11 @@ const PORT = process.env.PORT
 const app = express()
 
 // MIDDLEWARE
+app.use(express.static('public'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(methodOverride('_method'))
 
 // ROUTES
 app.get('/', (req, res) => {
@@ -19,6 +22,11 @@ app.get('/', (req, res) => {
 // Breads
 const breadController = require('./controllers/breads_controller.js')
 app.use('/breads', breadController)
+
+// 404 Page
+app.get('*', (req, res) => {
+    res.send('404')
+  })
 
 // LISTEN
 app.listen(PORT, () => {
