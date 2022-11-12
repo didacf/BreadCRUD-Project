405421e0b7a0472c6e890d0e06faf6b8624aa4d8
Didac Fernandez <didac.fernandez@insiemecloud.net>
2022-11-12 15:07:36 -0800

    Added mongoose and env

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 // DEPENDENCIES
 const express = require('express')
+const mongoose = require('mongoose')
 const methodOverride = require('method-override')
+
 
 // CONFIGURATION
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+  () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+)
+
 
 // MIDDLEWARE
 app.use(express.static('public'))
--- views/show.jsx
+++ views/show.jsx
@@ -14,6 +14,7 @@ function Show ({bread, index}) {
     }
     have gluten.
   </p>
+  <a href={`/breads/${index}/edit`}><button>Edit</button></a>
   <img src={bread.image} alt={bread.name} />
     <form action={`/breads/${index}?_method=DELETE`} method="POST">
        <input type='submit' value="DELETE"/>
