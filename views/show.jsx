cf105e295279d85d796b94f3dfcb360dfa6ef6f0
Didac Fernandez <didac.fernandez@insiemecloud.net>
2022-11-13 11:03:52 -0800

    Lesson 7 and 8

--- views/show.jsx
+++ views/show.jsx
@@ -16,6 +16,7 @@ function Show ({bread, index}) {
   </p>
   <a href={`/breads/${bread.id}/edit`}><button>Edit</button></a>
   <img src={bread.image} alt={bread.name} />
+    <p>Baked by {bread.baker}</p>
     <form action={`/breads/${bread.id}?_method=DELETE`} method="POST">
       <input type='submit' value="DELETE"/>
     </form>
